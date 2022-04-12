d2df75a3e380e34302106543881bf33530b42b83
EmanueleRangel <emanuele.rangel52@gmail.com>
2022-04-11 23:21:11 -0300

    products post and delete method created

--- api/rotas/fornecedores/produtos/index.js
+++ api/rotas/fornecedores/produtos/index.js
@@ -1,11 +1,34 @@
 const roteador = require('express').Router({ mergeParams: true })
 const Tabela = require('./TabelaProdutos')
+const Produto = require('./Produto')
 
 roteador.get('/', async (requsicao, resposta) => {
     const produtos = await Tabela.listar(requsicao.params.idFornecedor)
     resposta.send(
         JSON.stringify(produtos)
     )
+})
+
+roteador.post('/', async (requisicao, resposta) => {
+    const idFornecedor = requisicao.params.idFornecedor
+    const corpo = requisicao.body
+    const dados = Object.assign({}, corpo, {fornecedor: idFornecedor})
+    const produto = new Produto(dados)
+    await produto.criar()
+    resposta.status(201)
+    resposta.send(produto)
+})
+
+roteador.delete('/:id', async (requisicao, resposta) => {
+    const dados = {
+        id: requisicao.params.id,
+        fornecedor: requisicao.params.idFornecedor,
+    }
+
+    const produto = new Produto(dados)
+    await produto.apagar()
+    resposta.status(204)
+    resposta.end()
 })
 
 module.exports = roteador
